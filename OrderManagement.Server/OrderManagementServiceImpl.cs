b208b828086264afe3c9674ee89bd3e1c1197074
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2020-06-16 23:05:40 -0300

    server streaming gRPC sample

---
+++ OrderManagement.Server/OrderManagementServiceImpl.cs
@@ -0,0 +1,44 @@
+﻿using Ecommerce;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Server
+{
+    public class OrderManagementServiceImpl : OrderManagementService.OrderManagementServiceBase
+    {
+        public async  override Task searchOrders(
+            StringValue request, 
+            IServerStreamWriter<Order> responseStream, 
+            ServerCallContext context)
+        {
+
+            var i = 0;
+            while (!context.CancellationToken.IsCancellationRequested && i < 10)
+            {
+                await Task.Delay(500);
+
+                var result = new Order
+                {
+                    Description = $"order item {i}",
+                    Id = i.ToString(),
+                    Price = new Random(10).Next() * i,
+                    Destination = request.Value
+                };
+                for (int x = 0; x < i; x++)
+                {
+                    result.Items.Add("Product 1");
+                } 
+
+                i++;
+                await responseStream.WriteAsync(result);
+
+            }
+
+        }
+    }
+}
---
+++ OrderManagementClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using Ecommerce;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Core.Utils;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OrderManagementClient
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            var channel = new Channel("192.168.0.5", 5001, ChannelCredentials.Insecure);
+            var client = new OrderManagementService.OrderManagementServiceClient(channel);
+
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+
+            var value = new StringValue { Value = "OrderId" };
+            using var streamCall = client.searchOrders(value);
+
+            try
+            {
+                 streamCall.ResponseStream.ForEachAsync( item  =>
+                     Task.Run(() => Console.WriteLine($"Id:{item.Id} | Price: {item.Price} | Description: {item.Description} | ProductCount:{item.Items.Count}")));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
